4020ff8f9f82bceeaea0097fd64b7d7be393cda1
Michael Lu <38804680+Yamazaki93@users.noreply.github.com>
2018-05-28 15:40:33 -0700

    Added About page
    Should resolve #30

--- app/main.js
+++ app/main.js
@@ -86,6 +86,12 @@ function createWindow() {
                         releaseNote.openReleaseNote();
                     }
                 },
+                {
+                    label: "About",
+                    click(item, focusedWindow) {
+                        releaseNote.openAboutPage();
+                    }
+                }
             ]
         }
     ];
