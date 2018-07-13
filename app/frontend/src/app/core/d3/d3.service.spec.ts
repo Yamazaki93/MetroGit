f2c20d0c71f25fd0771556cbbe153379aa079a05
Michael Lu <38804680+Yamazaki93@users.noreply.github.com>
2018-07-12 21:18:50 -0700

    Fixed more unit test

--- app/frontend/src/app/core/d3/d3.service.spec.ts
+++ app/frontend/src/app/core/d3/d3.service.spec.ts
@@ -1,11 +1,16 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { D3Service } from './d3.service';
+import { MockCIIntegration } from '../mocks/mock-ci-integration-service';
+import { CiIntegrationService } from '../services/ci-integration.service';
 
 describe('D3Service', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [D3Service]
+      providers: [
+        D3Service,
+        { provide: CiIntegrationService, useClass: MockCIIntegration }
+      ]
     });
   });
 
--- app/frontend/src/app/core/git-view/git-view.component.spec.ts
+++ app/frontend/src/app/core/git-view/git-view.component.spec.ts
@@ -4,6 +4,21 @@ import { GitViewComponent } from './git-view.component';
 import { BranchViewerComponent } from '../branch-viewer/branch-viewer.component';
 import { SubwayComponent } from '../subway/subway.component';
 import { SubwayStationsComponent } from '../subway-stations/subway-stations.component';
+import { RepoService } from '../services/repo.service';
+import { MockRepo } from '../mocks/mock-repo-service';
+import { LoadingService } from '../../infrastructure/loading-service.service';
+import { MockLoading } from '../../infrastructure/mocks/mock-loading-service';
+import { NO_ERRORS_SCHEMA } from '../../../../node_modules/@angular/core';
+import { ContextMenuModule } from '../../../../node_modules/ngx-contextmenu';
+import { RouterTestingModule } from '../../../../node_modules/@angular/router/testing';
+import { LayoutService } from '../services/layout.service';
+import { MockLayout } from '../mocks/mock-layout-service';
+import { D3Service } from '../d3/d3.service';
+import { MockD3 } from '../mocks/mock-d3-service';
+import { SubmodulesService } from '../services/submodules.service';
+import { MockSubmodule } from '../mocks/mock-submodule-service';
+import { UpdaterService } from '../../infrastructure/updater.service';
+import { MockUpdater } from '../../infrastructure/mocks/mock-updater-service';
 
 describe('GitViewComponent', () => {
   let component: GitViewComponent;
@@ -11,9 +26,22 @@ describe('GitViewComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ GitViewComponent, BranchViewerComponent, SubwayComponent, SubwayStationsComponent ]
+      declarations: [GitViewComponent, BranchViewerComponent, SubwayComponent, SubwayStationsComponent],
+      imports: [
+        ContextMenuModule,
+        RouterTestingModule
+      ],
+      providers: [
+        { provide: RepoService, useClass: MockRepo },
+        { provide: LoadingService, useClass: MockLoading },
+        { provide: LayoutService, useClass: MockLayout },
+        { provide: D3Service, useClass: MockD3 },
+        { provide: SubmodulesService, useClass: MockSubmodule },
+        { provide: UpdaterService, useClass: MockUpdater}
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
