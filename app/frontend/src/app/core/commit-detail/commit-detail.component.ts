0d2e4578b6c15e956fd10d1b124250d74c10818c
anspage® <39483261+ansspage@users.noreply.github.com>
2019-08-15 09:10:21 +0300

    Revert "0.4.1"

--- app/frontend/src/app/core/commit-detail/commit-detail.component.ts
+++ app/frontend/src/app/core/commit-detail/commit-detail.component.ts
@@ -43,7 +43,6 @@ export class CommitDetailComponent implements OnInit {
         }
       } else {
         this.toggled = false;
-        this.selectedTab = "";
         this.closeFilePanel();
         this.layout.isDetailPanelOpen = false;
       }
